99deafee1cb5196dd7d9788b21a365a325d25f59
abdelilah ouaadouch <abdelilah4dev@gmail.com>
2026-04-20 18:55:20 +0000

    refactor: normalize generated entity patches

--- scripts/patch-entities.ps1
+++ scripts/patch-entities.ps1
@@ -14,15 +14,22 @@ $replacement = @"
 impl ActiveModelBehavior for ActiveModel {
     fn new() -> Self {
         Self {
-            id: sea_orm::ActiveValue::Set(ulid::Ulid::new().to_string()),
+            id: Set(ulid::Ulid::new().to_string()),
             ..Default::default()
         }
     }
 }
 "@
 
+$oldImport = "use sea_orm::entity::prelude::*;"
+$newImport = "use sea_orm::{Set, entity::prelude::*};"
+
 foreach ($file in $files) {
     $content = Get-Content -LiteralPath $file.FullName -Raw
+    if ($content.Contains($oldImport)) {
+        $content = $content.Replace($oldImport, $newImport)
+    }
+
     $marker = "impl ActiveModelBehavior for ActiveModel"
     $start = $content.IndexOf($marker)
 
